452346ac8c931c1f2eef3914df983e73a26639e5
Tom Powell <tompowell@gmail.com>
2015-02-22 10:36:27 -0500

    added validations and autohtml

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
   
 
-  resources :tasks
+  resources :tasks do
+    member do
+      put :change
+    end
+  end
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
- root 'pages#home'
- get 'about' => "pages#about"
- 
+  root 'pages#home'
+  get 'about' => "pages#about"
+  get 'test' => "pages#test"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
